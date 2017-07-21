8507ad7e14b8ed650654a78c3e0bb5884c2038ac
Mush <mush.harutyunyan@hotmail.com>
2017-07-21 16:53:27 +0400

    add jackpot urls

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,9 +9,19 @@ use App\Models\SuperenaLotto;
 use App\Models\FloridaLotto;
 class ApiController extends Controller
 {
-    protected $providers = array('PowerBall' => 'https://www.lotto.net/powerball/numbers',
-                                 'MegaMillions' => 'https://www.lotto.net/mega-millions/numbers',
-                                 'EuroMillions' => 'https://www.lotto.net/euromillions/results');
+    protected $providers = array(
+        'PowerBall' => 'https://www.lotto.net/powerball/numbers',
+         'MegaMillions' => 'https://www.lotto.net/mega-millions/numbers',
+         'EuroMillions' => 'https://www.lotto.net/euromillions/results',
+         'SuperEnaLotto' => 'https://www.lotto.net/superenalotto/results',
+         'EuroJackpot' => 'https://www.lotto.net/eurojackpot/results',
+//         'FloridaLotto' => 'https://www.lotto.net/florida-lotto/numbers',
+//         'CaliforniaSuperLotto' => 'https://www.lotto.net/california-super-lotto-plus/numbers',
+//         'OzLotto' => 'https://www.lotto.net/oz-lotto/results',
+//         'U.K.Lotto' => 'https://www.lotto.net/uk-lotto/results',
+//         'Lotto649' => 'https://www.lotto.net/canada-lotto-6-49/numbers',
+//         'AustraliaPowerBall' => 'https://www.lotto.net/australia-powerball/results'
+    );
 
     public function jackpot(){
         $client = new Client();
