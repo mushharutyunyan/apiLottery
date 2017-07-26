89ff6cba6fbbed6953d0c642fefa4d081b24786d
Mush <mush.harutyunyan@hotmail.com>
2017-07-26 17:35:44 +0400

    change api

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:api');
 
 
-//Route::group(['prefix' => 'jackpot','middleware' => 'auth:api'],function () {
-//    Route::get('/','ApiController@jackpot');
-//    Route::get('/results/{provider}','ApiController@results');
-//});
+Route::group(['prefix' => 'jackpot','middleware' => 'auth:api'],function () {
+    Route::get('/','ApiController@jackpot');
+    Route::get('/results/{provider}','ApiController@results');
+});
