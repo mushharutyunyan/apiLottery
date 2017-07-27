deb9d40ac614639b7ec8cc6540504d0df5d09804
Mush <mush.harutyunyan@hotmail.com>
2017-07-27 15:05:57 +0400

    add two providers

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -80,7 +80,6 @@ class ApiController extends Controller
             }
             $j++;
         }
-        print_r(count($providers));die;
         return response()->json($result);
     }
 }
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,11 +16,11 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-//        Log::info('Request Ip: '.Request::ip());
-//        if(Request::ip() != '46.166.180.153' && Request::ip() != '79.179.56.196'){
-//            return response()->json(false);
-//        }
-//        Log::info('Success Ip: '.Request::ip());
+        Log::info('Request Ip: '.Request::ip());
+        if(Request::ip() != '46.166.180.153' && Request::ip() != '79.179.56.196'){
+            return response()->json(false);
+        }
+        Log::info('Success Ip: '.Request::ip());
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
