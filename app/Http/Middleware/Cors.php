b66b52c9892b9952808ff642a6575ab6020b0fee
Mush <mush.harutyunyan@hotmail.com>
2017-07-27 12:49:57 +0400

    secure api

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-
+use Request;
 class Cors
 {
     /**
@@ -15,6 +15,9 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
+        if(Request::ip() != '46.166.180.153'){
+            return response()->json(false);
+        }
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
