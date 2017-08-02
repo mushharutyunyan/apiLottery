5e43e36cb587da4404f16f1c4eb4e90b99c1c03b
Mush <mush.harutyunyan@hotmail.com>
2017-08-02 15:28:47 +0400

    change token logic

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
+                    <ul class="nav navbar-nav navbar-right" style="font-size: 14px;">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ url('/login') }}">Login</a></li>
@@ -60,7 +60,7 @@
                         @else
                             <li><a href="{{ url('/plans') }}">Plans</a></li>
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style="font-size: 14px;">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" >
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
