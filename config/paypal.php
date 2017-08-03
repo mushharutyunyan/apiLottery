b31a391efe7c82b80794a805a728b6fcbf699320
Mush <mush.harutyunyan@hotmail.com>
2017-08-03 17:51:25 +0400

    changes

--- config/paypal.php
+++ config/paypal.php
@@ -1,8 +1,10 @@
 <?php
 return array(
     /** set your paypal credential **/
-    'client_id' =>'AT4Xp1jh8-bJc5_-kBlVsFoCQEUSXLNoW-EOejtzDHWSsRIWV-VuRpGo5Xy18PahVj1__xxYEO-dyMhz',
-    'secret' => 'EBLtHYGM18mjcWLKvBN-_ML49IM7zkBDKoGxQVsavuBOe5zxADwdeMa0UA8rF3sMzLZANrVJ9hvFdNqu',
+//    'client_id' =>'AT4Xp1jh8-bJc5_-kBlVsFoCQEUSXLNoW-EOejtzDHWSsRIWV-VuRpGo5Xy18PahVj1__xxYEO-dyMhz',
+//    'secret' => 'EBLtHYGM18mjcWLKvBN-_ML49IM7zkBDKoGxQVsavuBOe5zxADwdeMa0UA8rF3sMzLZANrVJ9hvFdNqu',
+    'client_id' =>'AavFy1PBhCzLC27KJLCgi9054wswB-AMJebBvPH3md5jR9AhI87XZYAKX-YedFhQikfPGuve4ApjJMCp',
+    'secret' => 'EMX5H8QrCHibfXLrzjK4h2n3EC7294qpEQYKrYc8iFdPNp8almQM52tG-3qEzyt8L4HjBe7up3ZanMHT',
     /**
      * SDK configuration
      */
@@ -10,7 +12,7 @@ return array(
         /**
          * Available option 'sandbox' or 'live'
          */
-        'mode' => 'live',
+        'mode' => 'sandbox',
         /**
          * Specify the max request time in seconds
          */
