ce9d73e8de85c9e867c8dc15b31ab9ca627e4ea3
Mush <mush.harutyunyan@hotmail.com>
2017-08-16 13:04:02 +0400

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
-Auth::routes();
 
+$this->get('/api/login', 'Auth\AuthController@showLoginForm');
+$this->post('login', 'Auth\AuthController@login');
+$this->get('logout', 'Auth\AuthController@logout');
 
+// Registration Routes...
+$this->get('register', 'Auth\AuthController@showRegistrationForm');
+$this->post('register', 'Auth\AuthController@register');
+
+// Password Reset Routes...
+$this->get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+$this->post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+$this->post('password/reset', 'Auth\PasswordController@reset');
 Route::group(['middleware' => 'checkAuth'],function () {
     Route::get('/', 'HomeController@index');
     Route::group(['middleware' => 'admin'],function () {
