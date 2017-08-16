ffe875852cea61a076ad882996fc68be637cfe2e
Mush <mush.harutyunyan@hotmail.com>
2017-08-16 13:06:35 +0400

    changes

--- routes/api.php
+++ routes/api.php
@@ -15,23 +15,6 @@ use Illuminate\Http\Request;
 
 
 
-Route::group(['middleware' => 'checkAuth'],function () {
-    Route::get('/', 'HomeController@index');
-    Route::group(['middleware' => 'admin'],function () {
-        Route::resource('/plan', 'PlansController');
-        Route::resource('/user', 'UserController');
-        Route::get('/plan/makeMain/{id}', 'PlansController@makeMain');
-    });
-    Route::get('/plans', 'HomeController@plans');
-    Route::get('/payments', 'HomeController@payments');
-    Route::group(['prefix' => 'payment'],function () {
-        Route::post('/payWithPaypal', 'PaymentController@payWithPaypal');
-        Route::get('/paypal/error', 'PaymentController@error');
-        Route::get('/paypal/{response}', 'PaymentController@getPaypalPaymentStatus');
-    });
-});
-
-Route::get('/verifyemail/{token}', 'Auth\RegisterController@verify');
 
 // API ROUTES
 Route::group(['prefix' => 'jackpot','middleware' => ['token.auth']],function () {
